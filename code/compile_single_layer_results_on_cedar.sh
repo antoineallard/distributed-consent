b70d630ec47736de547e8e0688b66ec53844dd08
antoineallard <antoine.allard.1@gmail.com>
2021-10-16 14:23:48 -0400

    added git support

--- code/compile_single_layer_results_on_cedar.sh
+++ code/compile_single_layer_results_on_cedar.sh
@@ -9,3 +9,7 @@ module load python
 module load scipy-stack
 
 python compile_single_layer_results.py
+
+git add ../results/single_layer/*.json
+git commit -m "updated single_layer json files"
+git push origin main
