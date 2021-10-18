9dd7e95e0c65eb1a6907df4c3996679253940577
antoineallard <antoine.allard.1@gmail.com>
2021-10-18 10:14:28 -0400

    added scripts for multilayer simulations

--- simulations/run_multilayer_simulations.sh
+++ simulations/run_multilayer_simulations.sh
@@ -19,31 +19,21 @@ nb_simulations=700
 
 
 # Compiles the binary file if it does not already exist.
-if [[ ! -f bin/emergence_of_components ]]; then
-  g++ -O3 -std=c++11 code/emergence_of_components.cpp -o bin/emergence_of_components
+if [[ ! -f bin/multilayer ]]; then
+  g++ -O3 -std=c++11 multilayer.cpp -o bin/multilayer
 fi
 
 
-# output_filename="../../../data_ethics_data/results/emergence_of_components/emergence_of_components.dat"
-# # if [[ -f $output_filename ]]; then rm $output_filename; fi
-
-
 # Loops over all available graphs.
-networks=(Dartmouth6 Brown11 UC64 William77 Williams40 Brandeis99 Maine59 UCSC68 Johns_Hopkins55 Vassar85 Vanderbilt48 Duke14 Georgetown15 Rice31 American75 USFCA72 Mich67 Colgate88 Carnegie49 Rochester38 UChicago30 Haverford76 Princeton12 Wesleyan43 Yale4 Caltech36 WashU32 Swarthmore42 Reed98 Simmons81 Bowdoin47 Tulane29 MIT8 Wake73 Pepperdine86 Hamilton46 Bucknell39 Emory27 Vermont70 Trinity100 Santa74 Middlebury45 Wellesley22 Tufts18 Howard90 Oberlin44 Smith60 Amherst41 Villanova62 Lehigh96)
+networks=(Dartmouth6 Brown11) # UC64 William77 Williams40 Brandeis99 Maine59 UCSC68 Johns_Hopkins55 Vassar85 Vanderbilt48 Duke14 Georgetown15 Rice31 American75 USFCA72 Mich67 Colgate88 Carnegie49 Rochester38 UChicago30 Haverford76 Princeton12 Wesleyan43 Yale4 Caltech36 WashU32 Swarthmore42 Reed98 Simmons81 Bowdoin47 Tulane29 MIT8 Wake73 Pepperdine86 Hamilton46 Bucknell39 Emory27 Vermont70 Trinity100 Santa74 Middlebury45 Wellesley22 Tufts18 Howard90 Oberlin44 Smith60 Amherst41 Villanova62 Lehigh96)
 for nname in ${networks[*]}; do
-
-  edgelist_filename=../../../data_ethics_data/Facebook100/${nname}.txt.tar.xz
+  edgelist_filename=../Facebook100/${nname}.txt.tar.xz
+# for edgelist_filename in ../Facebook100/*.txt.tar.xz; do
 
   network_name=${edgelist_filename##*/}
   network_name=${network_name%%.*}
 
-  output_filename=../../../data_ethics_data/results/importance_of_passports/${network_name}.dat
-  # if [[ -f $output_filename ]]; then rm $output_filename; fi
-
-  if [[ ! -f ${network_name}_empty.tmp ]]; then
-      touch ${network_name}_empty.tmp
-      echo ${network_name}
-  fi
+  output_filename=../results/multilayer/${network_name}.dat
 
   # Prints the header of the file if the files does not already exist.
   if [[ ! -f $output_filename ]]; then
@@ -73,16 +63,26 @@ for nname in ${networks[*]}; do
   fi
 
   # Uncompiles the archive containing the edgelist
-  tar xJf ${edgelist_filename}
-  # tar xJf ${dirname}/${network_name}.txt.tar.xz
+  if [[ ! -f ../Facebook100/${network_name}.txt ]]; then
+    echo 'extracting edgelist for '${network_name}
+    tar xJf ${edgelist_filename}
+    mv ${network_name}.txt ../Facebook100/
+  fi
+
   # Runs the script.
   for observation_depth_L in ${observation_depth_L_values[*]}; do
     for app_coverage in ${app_coverage_values[*]}; do
       for fraction_of_private_profiles in ${fraction_of_private_profiles_values[*]}; do
-        for adoption_within_private_profiles in ${adoption_within_private_profiles_values[*]}; do
-          for adoption_of_passports in 0.50 0.90 0.95 1.00; do
-            ./bin/emergence_of_components ${network_name}.txt $observation_depth_L $app_coverage $fraction_of_private_profiles $adoption_within_private_profiles $adoption_of_passports $nb_simulations $network_name >> $output_filename
-          done
+        for adoption_of_passports in 0.50 0.90 0.95 1.00; do
+
+          # When running simulations on a Compute Canada cluster.
+          bash launch_multilayer_simulations_on_cedar.sh $network_name $observation_depth_L $app_coverage $fraction_of_private_profiles $adoption_of_passports $nb_simulations $output_filename 0 0.025 1.000000001
+
+          # # When running simulations locally.
+          # for adoption_within_private_profiles in ${adoption_within_private_profiles_values[*]}; do
+          #   bin/emergence_of_components ${network_name}.txt $observation_depth_L $app_coverage $fraction_of_private_profiles $adoption_within_private_profiles $adoption_of_passports $nb_simulations $network_name >> $output_filename
+          # done
+
         done
       done
     done
