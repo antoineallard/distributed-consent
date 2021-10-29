351d26e00f8f2c8747dc4aed4ba815603863993a
antoineallard <antoine.allard.1@gmail.com>
2021-10-29 18:06:41 -0400

    Better columns names

--- simulations/compile_single_layer_results.py
+++ simulations/compile_single_layer_results.py
@@ -16,11 +16,10 @@ for results_filename in glob.glob("../results/single_layer/*.dat"):
     header = open(results_filename, 'r').readline().replace('#', ' ').split()
     df = pd.read_table(results_filename, names=header, comment="#", delimiter=r"\s+")
 
-    df["counter_culture"] = (df["NObsGCNbType0"] + df["NObsGCNbType1"] + df["NObsGCNbType2"]) / df["NbVertices"]
-    df["herd_immunity"] = (df["ObsNbType1"] / (df["ObsNbType1"] + df["NObsNbType1"]))
-    df["herd_immunity2"] = (df["ObsNbType2"] / (df["ObsNbType2"] + df["NObsNbType2"]))
-    df["obs_comp"] = (df["ObsNbType0"] + df["ObsNbType1"] + df["ObsNbType2"]) / df["NbVertices"]
+    df["CounterCulture"] = (df["NObsGCNbType0"] + df["NObsGCNbType1"] + df["NObsGCNbType2"]) / df["NbVertices"]
+    df["HerdImmunityType1"] = (df["ObsNbType1"] / (df["ObsNbType1"] + df["NObsNbType1"]))
+    df["HerdImmunityType2"] = (df["ObsNbType2"] / (df["ObsNbType2"] + df["NObsNbType2"]))
+    df["ObsCompRelSize"] = (df["ObsNbType0"] + df["ObsNbType1"] + df["ObsNbType2"]) / df["NbVertices"]
 
-    pt = df.pivot_table(columns=["ObsDepth", "PrivProfFrac", "AppCoverage", "AdoptionRate"], values = ["counter_culture", "herd_immunity", "herd_immunity2", "obs_comp"], aggfunc = [np.mean])
-    # pt.to_json(results_filename.rsplit(".", 1)[-2] + '.json')
+    pt = df.pivot_table(columns=["ObsDepth", "PrivProfFrac", "AppCoverage", "AdoptionRate"], values = ["CounterCulture", "HerdImmunityType1", "HerdImmunityType2", "ObsCompRelSize"], aggfunc = [np.mean])
     pt.to_pickle(results_filename.rsplit(".", 1)[-2] + '.pkl', compression="xz")
