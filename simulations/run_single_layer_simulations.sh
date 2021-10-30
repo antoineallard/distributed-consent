7c553ddf825200eac90d92827235698bb9cd4a83
antoineallard <antoine.allard.1@gmail.com>
2021-10-30 10:24:32 -0400

    updated README

--- simulations/run_single_layer_simulations.sh
+++ simulations/run_single_layer_simulations.sh
@@ -4,6 +4,7 @@
 # antoineallard.info
 # Octobre 2021
 
+
 observation_depth_L_values=(2)
 
 app_coverage_values=(0.01 0.025 0.005 0.001 0.0005 0.0001)
@@ -20,10 +21,7 @@ if [[ ! -f bin/single_layer ]]; then
 fi
 
 
-# Loops oµver all available graphs.
-# networks=(Dartmouth6 Brown11) # UC64 William77 Williams40 Brandeis99 Maine59 UCSC68 Johns_Hopkins55 Vassar85 Vanderbilt48 Duke14 Georgetown15 Rice31 American75 USFCA72 Mich67 Colgate88 Carnegie49 Rochester38 UChicago30 Haverford76 Princeton12 Wesleyan43 Yale4 Caltech36 WashU32 Swarthmore42 Reed98 Simmons81 Bowdoin47 Tulane29 MIT8 Wake73 Pepperdine86 Hamilton46 Bucknell39 Emory27 Vermont70 Trinity100 Santa74 Middlebury45 Wellesley22 Tufts18 Howard90 Oberlin44 Smith60 Amherst41 Villanova62 Lehigh96)
-# for nname in ${networks[*]}; do
-#   edgelist_filename=../Facebook100/${nname}.txt.tar.xz
+# Loops over all available graphs.
 for edgelist_filename in ../Facebook100/*.txt.tar.xz; do
 
   network_name=${edgelist_filename##*/}
@@ -69,14 +67,14 @@ for edgelist_filename in ../Facebook100/*.txt.tar.xz; do
     for app_coverage in ${app_coverage_values[*]}; do
       for fraction_of_private_profiles in ${fraction_of_private_profiles_values[*]}; do
 
-        # When running simulations on a Compute Canada cluster.
-        bash launch_single_layer_simulations_on_cedar.sh $network_name $observation_depth_L $app_coverage $fraction_of_private_profiles $nb_simulations $output_filename 0 0.025 1.000000001
-
         # # When running simulations locally.
         # adoption_within_private_profiles_values=$(seq 0 0.025 1.000000001)
         # for adoption_within_private_profiles in ${adoption_within_private_profiles_values[*]}; do
         #   bin/single_layer ${network_name}.txt $observation_depth_L $app_coverage $fraction_of_private_profiles $adoption_within_private_profiles $nb_simulations $network_name >> $output_filename
         # done
+
+        # When running simulations on a Compute Canada cluster.
+        bash launch_single_layer_simulations_on_cedar.sh $network_name $observation_depth_L $app_coverage $fraction_of_private_profiles $nb_simulations $output_filename 0 0.025 1.000000001
 
       done
     done
