0f3d5181511a1791534196995811592f61e5aba9
antoineallard <antoine.allard.1@gmail.com>
2021-11-28 19:17:16 -0500

    ignoring longer lines

--- simulations/compile_single_layer_results.py
+++ simulations/compile_single_layer_results.py
@@ -14,22 +14,30 @@ for results_filename in glob.glob("../results/single_layer/*.dat"):
     print("Updating " + results_filename.rsplit(".", 1)[-2] + '.pkl')
 
     header = open(results_filename, 'r').readline().replace('#', ' ').split()
+    header.append("NaN?")
     df = pd.read_table(results_filename, names=header, comment="#", delimiter=r"\s+", on_bad_lines='warn')
 
     if len(df['NbVertices'].unique()) > 1:
         d = df['NbVertices'].value_counts().to_dict()
         del d[max(d, key=lambda k: d[k])]
-    df.drop(df[df['NbVertices'].isin(d.keys())].index, inplace=True)
+        row_idx_to_remove = df[df['NbVertices'].isin(d.keys())].index
+        df.drop(row_idx_to_remove, inplace=True)
 
     if len(df['Name'].unique()) > 1:
         d = df['Name'].value_counts().to_dict()
         del d[max(d, key=lambda k: d[k])]
-    df.drop(df[df['Name'].isin(d.keys())].index, inplace=True)
+        row_idx_to_remove = df[df['Name'].isin(d.keys())].index
+        df.drop(row_idx_to_remove, inplace=True)
 
-    df["CounterCulture"] = (df["NObsGCNbType0"] + df["NObsGCNbType1"] + df["NObsGCNbType2"]) / df["NbVertices"]
-    df["HerdImmunityType1"] = (df["ObsNbType1"] / (df["ObsNbType1"] + df["NObsNbType1"]))
-    df["HerdImmunityType2"] = (df["ObsNbType2"] / (df["ObsNbType2"] + df["NObsNbType2"]))
-    df["ObsCompRelSize"] = (df["ObsNbType0"] + df["ObsNbType1"] + df["ObsNbType2"]) / df["NbVertices"]
+    if len(df['NaN?'].unique()) > 1:
+        row_idx_to_remove = df[~df['NaN?'].isna()].index
+        df.drop(row_idx_to_remove, inplace=True)
+    df.drop(columns=['NaN?'], inplace=True)
+
+    df["CounterCulture"] = (df["NObsGCNbType0"].astype('float64') + df["NObsGCNbType1"].astype('float64') + df["NObsGCNbType2"].astype('float64')) / df["NbVertices"].astype('float64')
+    df["HerdImmunityType1"] = (df["ObsNbType1"].astype('float64') / (df["ObsNbType1"].astype('float64') + df["NObsNbType1"].astype('float64')))
+    df["HerdImmunityType2"] = (df["ObsNbType2"].astype('float64') / (df["ObsNbType2"].astype('float64') + df["NObsNbType2"].astype('float64')))
+    df["ObsCompRelSize"] = (df["ObsNbType0"].astype('float64') + df["ObsNbType1"].astype('float64') + df["ObsNbType2"].astype('float64')) / df["NbVertices"].astype('float64')
 
     pt = df.pivot_table(columns=["ObsDepth", "PrivProfFrac", "AppCoverage", "AdoptionRate"],
                         values = ["CounterCulture", "HerdImmunityType1", "HerdImmunityType2", "ObsCompRelSize"],
